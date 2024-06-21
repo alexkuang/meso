051cedbb8f9bb3bc161f91faee89e64c7ebda57c
alex <alex@bikeshed.coffee>
2024-06-20 20:47:51 -0400

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,26 @@
 defmodule Meso.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/alexkuang/meso"
+
   def project do
     [
       app: :meso,
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Nothing to see here yet",
+      package: package(),
+      source_url: @source_url
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Alex Kuang"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 
@@ -21,6 +34,7 @@ defmodule Meso.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
